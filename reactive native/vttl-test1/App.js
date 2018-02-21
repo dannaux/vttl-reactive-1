e195699487d87b4b976b9b388c3d3012d2cd17ad
Philip Dannaux <philip@dannaux-consulting.be>
2018-02-21 22:51:58 +0100

    load player based on UniqueIndex

--- reactive native/vttl-test1/App.js
+++ reactive native/vttl-test1/App.js
@@ -14,38 +14,64 @@ function loadMeerdaal() {
     });
 }
 
-function loadPlayer(uniqueIndex: String) {
-  var soap = require('soap-everywhere');
-  var url = 'http://api.vttl.be/0.7/?WSDL';
-  var args = { Season: 18, UniqueIndex: uniqueIndex, WithResults: 'FALSE'};
 
 
-    soap.createClient(url, function(err, client) {
-        client.GetMembers(args, function(err, result) {
-            console.log(result);
-        });
-    });
-}
-
-loadPlayer('522436');
 
   export class Player extends Component {
 
+    loadPlayer(uniqueIndex: String) {
+      var self = this;
+      var soap = require('soap-everywhere');
+      var url = 'http://api.vttl.be/0.7/?WSDL';
+      var args = { Season: 18, UniqueIndex: uniqueIndex, WithResults: 'FALSE'};
+
+        soap.createClient(url, function(err, client) {
+            client.GetMembers(args, function(err, response) {
+                self.setState({ player: self.convertPlayerResponse(response) })
+            });
+        });
+    }
+
+    convertPlayerResponse(playerResponse) {
+      return playerResponse.MemberEntries[0]
+    }
+
+    constructor(props) {
+      super(props);
+      this.state = {
+        player: null
+      };
+    }
+
+    componentDidMount()  {
+      this.loadPlayer('522436');
+    }
+
     render() {
-       this.playerImageUri = this.computeImagePlayerUri(this.props);
-      return (
-        <View style={{flexDirection: 'row', justifyContent: 'center', marginTop: 50}}>
-          <Image
-            style={{width: 200, height: 200}}
-           source={{uri: this.playerImageUri}}
-           />
-           <View style={{flexDirection: 'column', alignItems: 'stretch', marginLeft: 20}}>
-            <Text style={styles.playerName}>{this.props.FirstName}</Text>
-            <Text style={styles.playerName}>{this.props.LastName}</Text>
-            <Text style={styles.ranking}>{this.props.Ranking}</Text>
+      var self = this;
+      if (this.state.player != null) {
+        this.playerImageUri = this.computeImagePlayerUri(this.props);
+       return (
+         <View style={{flexDirection: 'row', justifyContent: 'center', marginTop: 50}}>
+           <Image
+             style={{width: 200, height: 200}}
+            source={{uri: this.playerImageUri}}
+            />
+            <View style={{flexDirection: 'column', alignItems: 'stretch', marginLeft: 20}}>
+             <Text style={styles.playerName}>{self.state.player.LastName}</Text>
+             <Text style={styles.playerName}>{self.state.player.FirstName}</Text>
+             <Text style={styles.ranking}>{self.state.player.Ranking}</Text>
+           </View>
+         </View>
+       );
+      }
+      else {
+        return (
+          <View style={{flexDirection: 'row', justifyContent: 'center', marginTop: 50}}>
+            <Text style={styles.playerName}>Loading player</Text>
           </View>
-        </View>
-      );
+        );
+      }
     }
 
     computeImagePlayerUri(playerProperties) {
