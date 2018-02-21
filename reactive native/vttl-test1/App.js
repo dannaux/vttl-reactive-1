67961ca327b1584aa3197030033bb1f895a0f5aa
Philip Dannaux <philip@dannaux-consulting.be>
2018-02-21 20:34:30 +0100

    styling + more generic code

--- reactive native/vttl-test1/App.js
+++ reactive native/vttl-test1/App.js
@@ -1,36 +1,68 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { StyleSheet, Text, View, Image, Button } from 'react-native';
 
-var soap = require('soap-everywhere');
-var url = 'http://api.vttl.be/0.7/?WSDL';
-var args = { Season: 18, Club: 'Vl-B234'};
+function loadMeerdaal() {
+  var soap = require('soap-everywhere');
+  var url = 'http://api.vttl.be/0.7/?WSDL';
+  var args = { Season: 18, Club: 'Vl-B234'};
 
 
-  soap.createClient(url, function(err, client) {
-      client.GetClubs(args, function(err, result) {
-          console.log(result);
-      });
-  });
-
-
-
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-        <Image
-          style={{width: 200, height: 200}}
-         source={{uri: 'https://competitie.vttl.be/photos/1718/small/Lander.Dannaux.522436.jpg'}}
-         />
-         <Text>This includes SOAP everywhere</Text>
-         <Text>This includes URL</Text>
-      </View>
-    );
-  }
+    soap.createClient(url, function(err, client) {
+        client.GetClubs(args, function(err, result) {
+            console.log(result);
+        });
+    });
 }
+
+function loadPlayer(uniqueIndex: String) {
+  var soap = require('soap-everywhere');
+  var url = 'http://api.vttl.be/0.7/?WSDL';
+  var args = { Season: 18, UniqueIndex: uniqueIndex, WithResults: 'FALSE'};
+
+
+    soap.createClient(url, function(err, client) {
+        client.GetMembers(args, function(err, result) {
+            console.log(result);
+        });
+    });
+}
+
+loadPlayer('522436');
+
+  export class Player extends Component {
+
+    render() {
+       this.playerImageUri = this.computeImagePlayerUri(this.props);
+      return (
+        <View style={{flexDirection: 'row', justifyContent: 'center', marginTop: 50}}>
+          <Image
+            style={{width: 200, height: 200}}
+           source={{uri: this.playerImageUri}}
+           />
+           <View style={{flexDirection: 'column', alignItems: 'stretch', marginLeft: 20}}>
+            <Text style={styles.playerName}>{this.props.FirstName}</Text>
+            <Text style={styles.playerName}>{this.props.LastName}</Text>
+            <Text style={styles.ranking}>{this.props.Ranking}</Text>
+          </View>
+        </View>
+      );
+    }
+
+    computeImagePlayerUri(playerProperties) {
+      return 'https://competitie.vttl.be/photos/1718/small/'+playerProperties.FirstName+'.'+playerProperties.LastName+'.'+playerProperties.UniqueIndex+'.jpg';
+    }
+  }
+
+  export default class App extends Component {
+    render() {
+      return (
+        <View>
+           <Player LastName="Dannaux" FirstName="Lander" UniqueIndex="522436" Ranking="NG"/>
+        </View>
+      );
+    }
+  }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -39,4 +71,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  ranking: {
+    fontWeight: 'bold',
+    fontSize: 40
+  },
+  playerName: {
+    fontSize: 30
+  }
 });
